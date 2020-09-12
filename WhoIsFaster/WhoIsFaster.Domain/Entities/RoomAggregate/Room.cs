cd093af3819c74515c574a8cadd45f90375702f6
filipjovasevic996 <filip.jovasevic96@gmail.com>
2020-09-12 13:02:29 +0200

    Implemented Room and RoomPlayer methods

--- WhoIsFaster/WhoIsFaster.Domain/Entities/RoomAggregate/Room.cs
+++ WhoIsFaster/WhoIsFaster.Domain/Entities/RoomAggregate/Room.cs
@@ -30,9 +30,10 @@ namespace WhoIsFaster.Domain.Entities.RoomAggregate
         public int TextId { get; private set; }
         public Text Text { get; private set; }
         public bool IsDeleted { get; private set; }
+        public byte[] Timestamp { get; set; }
 
         public Room()
-        { 
+        {
         }
 
         public Room(int maxPlayers, int playersToStart, Text text, double gameLengthSeconds, double lengthOfStarting, RoomType roomType)
@@ -48,6 +49,146 @@ namespace WhoIsFaster.Domain.Entities.RoomAggregate
             IsDeleted = false;
         }
 
+        public void UpdateRoom(Room room)
+        {
+            HasStarted = room.HasStarted;
+            HasFinished = room.HasFinished;
+            TimeStarted = room.TimeStarted;
+            LastPlayerJoined = room.LastPlayerJoined;
+            foreach (RoomPlayer roomPlayer in RoomPlayers)
+            {
+                roomPlayer.UpdateRoomPlayer(room.RoomPlayers.FirstOrDefault(rp => rp.Id == roomPlayer.Id));
+            }
+        }
+
+        public bool PlayerJoin(RegularUser regularUser)
+        {
+            var joined = false;
+            if (!HasStarted || !IsFull())
+            {
+                joined = true;
+                roomPlayers.Add(new RoomPlayer(this, regularUser, WordList[0]));
+                LastPlayerJoined = DateTime.Now;
+            }
+
+            return joined;
+        }
+
+        public void Start()
+        {
+            HasStarted = true;
+            TimeStarted = DateTime.Now;
+        }
+
+
+        public void SetIsStarting()
+        {
+            IsStarting = true;
+            StartEventTime = DateTime.Now;
+        }
+
+
+        public void ChangeText(Text newText)
+        {
+            if (!HasStarted)
+            {
+                WordList = newText.TextContent.Split(" ").ToList();
+                Text = newText;
+            }
+        }
+
+        public bool IsFull()
+        {
+            return roomPlayers.Count == MaxPlayers;
+        }
+
+
+        public void UpdateRoomPlayers()
+        {
+            if (!HasFinished)
+            {
+
+                foreach (var player in roomPlayers)
+                {
+                    UpdateRoomPlayer(player);
+                }
+            }
+        }
+
+        public void UpdateRoomPlayerInput(string userName, string input)
+        {
+            if (!HasFinished)
+            {
+                var player = roomPlayers.FirstOrDefault(rp => rp.UserName == userName);
+                player.UpdateCurrentInput(input);
+            }
+        }
+
+        public void UpdateRoomPlayer(RoomPlayer player)
+        {
+            if (!HasFinished)
+            {
+                var FinishedWord = player.CheckInput();
+                if (FinishedWord)
+                {
+                    player.UpdateCurrentWord(WordList[player.CorrectlyTypedWordNumber]);
+                    player.UpdateWordsPerMinute(Convert.ToInt32(player.CorrectlyTypedWordNumber / (DateTime.Now - TimeStarted).TotalMinutes));
+                }
+            }
+        }
+
+        public bool CheckIfOver()
+        {
+            var isOver = false;
+            if ((DateTime.Now - TimeStarted).TotalSeconds >= GameLengthSeconds)
+            {
+                isOver = true;
+                HasFinished = true;
+                int maxWords = -1;
+                RoomPlayer maxWordPlayer = null;
+                foreach (var player in roomPlayers)
+                {
+                    if (player.CorrectlyTypedWordNumber > maxWords)
+                    {
+                        maxWords = player.CorrectlyTypedWordNumber;
+                        maxWordPlayer = player;
+                    }
+                }
+                maxWordPlayer.PlayerWon();
+            }
+            else
+            {
+                foreach (var player in roomPlayers)
+                {
+                    if (player.CorrectlyTypedWordNumber == this.WordList.Count())
+                    {
+                        player.PlayerWon();
+                        isOver = true;
+                        HasFinished = true;
+                    }
+                }
+            }
+            return isOver;
+        }
+
+        public bool ShouldStart()
+        {
+            if (RoomType == RoomType.Public && roomPlayers.Count >= PlayersToStart && (DateTime.Now - LastPlayerJoined).TotalSeconds >= LengthOfStarting)
+            {
+                return true;
+            }
+            else if (RoomType != RoomType.Public && IsStarting && (DateTime.Now - StartEventTime).TotalSeconds >= LengthOfStarting)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void SetWordList()
+        {
+            WordList = Text.TextContent.Split(" ").ToList();
+        }
 
         public void Delete()
         {
--- WhoIsFaster/WhoIsFaster.Domain/Entities/RoomAggregate/RoomPlayer.cs
+++ WhoIsFaster/WhoIsFaster.Domain/Entities/RoomAggregate/RoomPlayer.cs
@@ -13,6 +13,9 @@ namespace WhoIsFaster.Domain.Entities.RoomAggregate
         public int RoomId { get; private set; }
         public Room Room { get; private set; }
         public int WordsPerMinute { get; private set; }
+        public int CorrectlyTypedTextIndex { get; private set; }
+        public int IncorrectilyTypedTextEndIndex { get; private set; }
+        public int CurrentTextIndex { get; private set; }
         public string CurrentWord { get; private set; }
         public int CorrectlyTypedWordNumber { get; private set; }
         public bool HasWon { get; private set; }
@@ -32,9 +35,92 @@ namespace WhoIsFaster.Domain.Entities.RoomAggregate
             RegularUser = regularUser;
             CurrentWord = currentWord;
             WordsPerMinute = 0;
+            CorrectlyTypedTextIndex = 0;
+            IncorrectilyTypedTextEndIndex = 0;
+            CurrentTextIndex = 0;
             CorrectlyTypedWordNumber = 0;
             HasWon = false;
             CurrentInput = "";
+        }
+
+        public void UpdateRoomPlayer(RoomPlayer roomPlayer)
+        {
+            WordsPerMinute = roomPlayer.WordsPerMinute;
+            CorrectlyTypedTextIndex = roomPlayer.CorrectlyTypedTextIndex;
+            IncorrectilyTypedTextEndIndex = roomPlayer.IncorrectilyTypedTextEndIndex;
+            CurrentTextIndex = roomPlayer.CurrentTextIndex;
+            CorrectlyTypedWordNumber = roomPlayer.CorrectlyTypedWordNumber;
+            HasWon = roomPlayer.HasWon;
+            CurrentInput = roomPlayer.CurrentInput;
+            IsRoomAdmin = roomPlayer.IsRoomAdmin;
+        }
+
+        public void UpdateWordsPerMinute(int wordsPerMinute)
+        {
+            WordsPerMinute = wordsPerMinute;
+        }
+        public void UpdateCorrectlyTypedTextIndex(int newCorrectlyTypedTextIndex)
+        {
+            CorrectlyTypedTextIndex = newCorrectlyTypedTextIndex;
+        }
+        public void UpdateIncorrectilyTypedTextEndIndex(int newIncorrectilyTypedTextEndIndex)
+        {
+            IncorrectilyTypedTextEndIndex = newIncorrectilyTypedTextEndIndex;
+        }
+
+        public void UpdateCurrentWord(string currentWord)
+        {
+            CurrentWord = currentWord;
+        }
+
+        public void UpdateCurrentInput(string currentInput)
+        {
+            CurrentInput = currentInput;
+        }
+
+        public void PlayerWon()
+        {
+            HasWon = true;
+        }
+
+        public void SetRoomAdmin()
+        {
+            IsRoomAdmin = true;
+        }
+
+        public bool CheckInput()
+        {
+            var correctIndex = GetCorrectlyTypedIndexOfWord(CurrentInput);
+            var FinishedWord = false;
+            if (correctIndex == CurrentWord.Length && CurrentInput[correctIndex] == ' ')
+            {
+                CurrentTextIndex = CurrentTextIndex + CurrentWord.Length + 1;
+                CorrectlyTypedTextIndex = CurrentTextIndex;
+                IncorrectilyTypedTextEndIndex = CurrentTextIndex;
+                CorrectlyTypedWordNumber += 1;
+                FinishedWord = true;
+            }
+            else
+            {
+                CorrectlyTypedTextIndex += correctIndex;
+                IncorrectilyTypedTextEndIndex += CurrentInput.Length;
+            }
+
+            return FinishedWord;
+        }
+
+        private int GetCorrectlyTypedIndexOfWord(string input)
+        {
+            int n = CurrentWord.Length;
+            int m = input.Trim().Length;
+            int i;
+            for (i = 0; i < Math.Min(n, m); i++)
+            {
+                if (CurrentWord[i] != input[i])
+                    break;
+            }
+
+            return i;
         }
 
     }
