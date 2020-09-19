99f07168e8163d661f90c0dd5fa2001b21a33e77
filipjovasevic996 <filip.jovasevic96@gmail.com>
2020-09-19 19:51:49 +0200

    Implemented GameService

---
+++ WhoIsFaster/WhoIsFaster.BlazorApp/GameServices/GameService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WhoIsFaster.BlazorApp.GameDefinitions;
+
+namespace WhoIsFaster.BlazorApp.GameServices
+{
+    class GameService : IGameService
+    {
+        private readonly Game game;
+
+        public GameService()
+        {
+            game = Game.Instance;
+        }
+        public void UpdateRoomPlayerInput(int roomId, string userName, string input)
+        {
+            game.UpdateRoomPlayerInput(roomId, userName, input);
+        }
+
+        public async Task AddRoomToGame(int roomId)
+        {
+            await game.AddRoom(roomId);
+        }
+    }
+}
---
+++ WhoIsFaster/WhoIsFaster.BlazorApp/GameServices/IGameService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhoIsFaster.BlazorApp.GameServices
+{
+    public interface IGameService
+    {
+        void UpdateRoomPlayerInput(int roomId, string userName, string input);
+        Task AddRoomToGame(int roomId);
+    }
+}
