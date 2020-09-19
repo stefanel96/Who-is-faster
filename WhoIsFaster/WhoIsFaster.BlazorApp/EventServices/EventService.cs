e09fceb50a16934cde85b54153dc129902fb94ba
stefanel96 <stefanlazovic1010@gmail.com>
2020-09-19 19:43:35 +0200

    Implemented EventService in BlazorApp

---
+++ WhoIsFaster/WhoIsFaster.BlazorApp/EventServices/EventService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WhoIsFaster.Infrastructure.SignalRNotifications.NotificationManagerInterfaces;
+
+namespace WhoIsFaster.BlazorApp.EventServices
+{
+    public class EventService : IEventService
+    {
+        private readonly IGameNotificationManager _NotificationManager;
+
+        public EventService(IGameNotificationManager NotificationManager)
+        {
+            _NotificationManager = NotificationManager;
+        }
+        public async Task AddConnectionToSignalRGroup(string connectionId, string groupName)
+        {
+            await _NotificationManager.AddConnectionToGroup(connectionId, groupName);
+        }
+    }
+}
