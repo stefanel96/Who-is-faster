4d5ccb56dc352a128406e12891f1d27d295eb913
lukamilosevic11 <lukamilosevic11@gmail.com>
2020-09-23 16:17:49 +0200

    contact us finished implemented sending email to support team, now users can reach support team through form

--- WhoIsFaster/WhoIsFaster.BlazorApp/Pages/Contact.cs
+++ WhoIsFaster/WhoIsFaster.BlazorApp/Pages/Contact.cs
@@ -1,16 +1,27 @@
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Configuration;
+using WhoIsFaster.BlazorApp.EmailServices;
 
 namespace WhoIsFaster.BlazorApp.Pages {
 
     public partial class Contact {
+        
+        [Inject]
+        public IConfiguration Configuration { get; set; }
+        [Inject]
+        public IEmailSender EmailSender { get; set; }
 
         public ContactFormModel _ContactFormModel { get; set; } = new ContactFormModel();
 
         private bool displaySendAlert { get; set; } = false;
 
-        protected void HandleValidSubmit()
+        protected async Task HandleValidSubmit()
         {
+            string body = "<h2>Reach client:</h2><ul><li>Email: "+_ContactFormModel.Email+"</li><li>Phone number: "+_ContactFormModel.Phone+"</li></ul>"
+                            +"<h2>Question/Message:</h2><p>"+_ContactFormModel.Message+"</p>";
+            await EmailSender.SendEmailAsync(Configuration["EmailSettings:Sender"],"Support(from contact us): "+_ContactFormModel.Name+" has a question",body);
             displaySendAlert = true;
         }
 
